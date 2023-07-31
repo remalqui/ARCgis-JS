c6a4d487cd4a02f9fd071f8c2f5ba906acbf2755
Ricardo9711 <rmt.97.11@outlook.es>
2023-07-31 15:51:39 -0500

    Muestra las rutas por vendor y por fecha

--- index.js
+++ index.js
@@ -3,18 +3,13 @@ import MapView from '@arcgis/core/views/MapView';
 import config from '@arcgis/core/config';
 import Graphic from '@arcgis/core/Graphic.js';
 import GraphicsLayer from '@arcgis/core/layers/GraphicsLayer.js';
-import Search from "@arcgis/core/widgets/Search.js";
-import Point from "@arcgis/core/geometry/Point.js";
+
 
 config.apiKey = 'AAPK5e0a32880aae4a70a5dc38877ee2846e7o3jkkVClLIrbPdDakzoq7B3_SjkF9FedMbVnCjcS-ealECOVG9ReuAr9EeSmVEL';
 let infoPanel;  // Info panel for place information
-let clickPoint;  // Clicked point on the map
-let activeCategory = "10000";  // Landmarks and Outdoors category
-
-// GraphicsLayer for places features
-const placesLayer = new GraphicsLayer({
-  id: "placesLayer"
-});
+let activeCategory = "0";  // Landmarks and Outdoors category
+let activeCategory1 = "0"; // Landmarks and Outdoors category
+let activeCategory2 = "0"; // Landmarks and Outdoors category
 
 // GraphicsLayer for map buffer
 const bufferLayer = new GraphicsLayer({
@@ -23,25 +18,25 @@ const bufferLayer = new GraphicsLayer({
 
 // Info panel interactions
 const categorySelect = document.getElementById("categorySelect");
+const categorySelectDate = document.getElementById("categorySelectDate");
 const resultPanel = document.getElementById("results");
 const flow = document.getElementById("flow");
 
 const map = new Map ({
   basemap: 'streets-navigation-vector',
-  layers : [bufferLayer, placesLayer]
+  layers : bufferLayer
 });
 
 const view = new MapView({
   map : map,
   container: 'viewDiv',
-  center: [-77.033695, -12.065190],
-  zoom: 13
+  center: [-77.043025 , -12.0809538],
+  zoom: 14
 });
 
     // Clear graphics and results from the previous place search
     function clearGraphics() {
       bufferLayer.removeAll();  // Remove graphics from GraphicsLayer of previous buffer
-      placesLayer.removeAll();  // Remove graphics from GraphicsLayer of previous places search
       resultPanel.innerHTML = "";
       if (infoPanel) infoPanel.remove();
     }
@@ -50,60 +45,123 @@ const view = new MapView({
     categorySelect.addEventListener("calciteComboboxChange", () => {
       activeCategory = categorySelect.value;
       clearGraphics();
+      actualizarCategoria ();
       // Pass point to the showPlaces() function with new category value
-      activeCategory && showPlaces(activeCategory);
+      //activeCategory && showPlaces(activeCategory);
     });
 
-    const polyline = {};
-    const simpleLineSymbol = {};
+    categorySelectDate.addEventListener("calciteComboboxChange", () => {
+      activeCategory1 = categorySelectDate.value;
+      clearGraphics();
+      actualizarCategoria ();
+      // Pass point to the showPlaces() function with new category value
+      //activeCategory1 && showPlaces(activeCategory);
+    });
+
+function actualizarCategoria (){
+switch (activeCategory) {
+  case "10000":
+    switch (activeCategory1) {
+      case "20000":
+        activeCategory2 = "30000";
+        showPlaces(activeCategory2);
+        break;
+      
+      case "21000":
+        activeCategory2 = "31000";
+        showPlaces(activeCategory2);   
+        break;
+
+      default:
+        break;
+    }
+    break;
+
+  case "11000":
+    switch (activeCategory1) {
+      case "20000":
+        activeCategory2 = "32000";
+        showPlaces(activeCategory2);         
+        break;
+      
+      case "21000":
+        activeCategory2 = "33000";
+        showPlaces(activeCategory2);           
+        break;
+  
+      default:
+        break;
+    }  
+    break;
+
+  default:
+    break;
+}}
+
+    var polyline = {};
+    var simpleLineSymbol = {};
     
         // Display map click search area and pass to places service
-        async function showPlaces(placePoint) {
+        async function showPlaces(placepoint) {
           
-          switch (placePoint) {
-            case "10000":
-                polyline.type = "polyline";
-                polyline.paths = [
-                     [ -77.033695, -12.065190 ],
-                     [ -77.035019, -12.066749 ],
-                     [ -77.038007, -12.067325 ],
-                     [ -77.038067, -12.067855 ],
-                     [ -77.038942, -12.069893 ]
-                ];
-                
-                simpleLineSymbol.type = "simple-line";
-                simpleLineSymbol.color = [226, 119, 40]; // Orange
-                simpleLineSymbol.width =  2;
-              break;
-              case "11000":
-                polyline.type = "polyline";
-                polyline.paths= [
-                     [ -77.055816 , -12.091007 ],
-                     [ -77.050850 , -12.088260 ],
-                     [ -77.049648 , -12.088438 ],
-                     [ -77.044544 , -12.087752 ],
-                     [ -77.044225 , -12.089623 ]
-                ];
-                simpleLineSymbol.type = "simple-line";
-                simpleLineSymbol.color = [47, 90, 226]; // blue
-                simpleLineSymbol.width =  2;
-              break;          
+          switch (placepoint) {
+            case "30000":
+                    polyline.type = "polyline";
+                    polyline.paths = [
+                         [ -77.033695, -12.065190 ],
+                         [ -77.035019, -12.066749 ],
+                         [ -77.038007, -12.067325 ],
+                         [ -77.038067, -12.067855 ],
+                         [ -77.038942, -12.069893 ]
+                    ];
+
+                    simpleLineSymbol.type = "simple-line";
+                    simpleLineSymbol.color = [255, 0, 0]; // red
+                    simpleLineSymbol.width =  2;
+                    break;
+            case "31000":
+                    polyline.type = "polyline";
+                    polyline.paths = [
+                         [ -77.047588 , -12.075061 ],
+                         [ -77.046033 , -12.075140 ],
+                         [ -77.044316 , -12.075162 ],
+                         [ -77.043947 , -12.074847 ],
+                         [ -77.044831 , -12.072980 ]
+                    ];
+
+                    simpleLineSymbol.type = "simple-line";
+                    simpleLineSymbol.color = [0, 0, 255]; // blue
+                    simpleLineSymbol.width =  2;
+                    break;
+            case "32000":
+                    polyline.type = "polyline";
+                    polyline.paths = [
+                         [ -77.055816 , -12.091007 ],
+                         [ -77.050850 , -12.088260 ],
+                         [ -77.049648 , -12.088438 ],
+                         [ -77.044544 , -12.087752 ],
+                         [ -77.044225 , -12.089623 ]
+                    ];
+
+                    simpleLineSymbol.type = "simple-line";
+                    simpleLineSymbol.color = [0,128,0]; // verde
+                    simpleLineSymbol.width =  2;
+                    break;
+            case "33000":
+                    polyline.type = "polyline";
+                    polyline.paths = [
+                         [ -77.042431 , -12.084649 ],
+                         [ -77.041985 , -12.087816 ],
+                         [ -77.042510 , -12.087948 ],
+                         [ -77.043677 , -12.087692 ],
+                         [ -77.045893 , -12.087860 ]
+                    ];
+  
+                    simpleLineSymbol.type = "simple-line";
+                    simpleLineSymbol.color = [0, 0, 0]; // negro
+                    simpleLineSymbol.width =  2;
+                    break;  
             default:
-              polyline = {
-                type: "polyline",
-                paths: [
-                   [ -77.033695, -12.065190 ],
-                   [ -77.035019, -12.066749 ],
-                   [ -77.038007, -12.067325 ],
-                   [ -77.038067, -12.067855 ],
-                   [ -77.038942, -12.069893 ]
-              ]
-              };
-              simpleLineSymbol = {
-                 type: "simple-line",
-                 color: [226, 119, 40], // Orange
-                 width: 2
-              };
               break;
           }
 
@@ -117,4 +175,5 @@ const view = new MapView({
 
 view.when(()=> {
     console.log('view ready');
+
 });
